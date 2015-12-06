df1d2388b9f4aaf7d76153b55212d8e40e699221
kannanharidas <kannanvariarh@gmail.com>
2015-12-06 15:53:33 -0700

    Validation file separately created for evaluation
    Signed-off-by: kannanharidas <kannanvariarh@gmail.com>

--- data_processing/image_pickling.py
+++ data_processing/image_pickling.py
@@ -40,12 +40,42 @@ def prepare_data_for_keras(file_path, label_path):
                 pic_label = files_label[file_name]
                 class_label.append(pic_label)
                 i = i+1
-                if i == 250:
+                if i == 100:
                     break
             feature = numpy.array(values)
             label = numpy.array(class_labels)
             print feature.shape
             print label.shape
+        except:
+            pass
+
+    return values, class_label
+
+def prepare_validation_data(file_path, label_path):
+    file_names =  os.listdir(file_path)
+    files_label = get_labels(label_path)
+
+    class_label = []
+    values = []
+    i = 0
+    for i in range(300,400):
+        file_name = file_names[i]
+        try:
+            pixel_array = numpy.asarray(Image.open(file_path+file_name))
+            x = pixel_array.shape
+            if x[0] == 256:
+                matrix_values = []
+                for h in range(x[0]):
+                    col_values = []
+                    for w in range(x[1]):
+                        rgb = pixel_array[h][w]
+                        mono = sum(rgb)
+                        col_values.append(int(mono))
+                    matrix_values.append(col_values)
+                values.append(matrix_values)
+                pic_label = files_label[file_name]
+                class_label.append(pic_label)
+                i = i+1
         except:
             pass
 
@@ -60,6 +90,20 @@ def dump_gzip(values,class_labels,zip_file_name):
 
     with gzip.open(zip_file_name, 'wb') as f:
         cPickle.dump((feature, label), f, cPickle.HIGHEST_PROTOCOL)
+
+def dump_validation_gzip(values, zip_file_name):
+    feature = numpy.array(values)
+    print feature.shape
+
+    with gzip.open(zip_file_name,'wb') as f:
+        cPickle.dump(feature,f,cPickle.HIGHEST_PROTOCOL)
+
+def write_validation_labels(labels, file_path):
+    output_file = open(file_path,'wb')
+    writer = csv.writer(output_file)
+    for label in labels:
+        writer.writerow([label])
+    output_file.close()
 
 def split_training_data(filename):
     f = gzip.open(filename, 'rb')
@@ -77,16 +121,24 @@ if __name__ == '__main__':
 
     testing_gzip_file_name = "testing_data.pkl.gz"
     training_gzip_file_name = "training_data.pkl.gz"
+    validation_gzip_file_name = "validation_data.pkl.gz"
+
+    validation_class_labels_file ="./../files/validation_files_labels.csv"
 
     print "Dumping for testing data"
     values , class_labels = prepare_data_for_keras(testing_files_path,testing_class_label_mapping_path)
     dump_gzip(values,class_labels,testing_gzip_file_name)
     print "Dumping done for testing data" + "\n"
 
-    # print "Dumping for training data"
-    # values , class_labels = prepare_data_for_keras(training_files_path,training_class_label_mapping_path)
-    # dump_gzip(values,class_labels,training_gzip_file_name)
-    # print "Dumping done for training data"
+    print "Dumping for training data"
+    values , class_labels = prepare_data_for_keras(training_files_path,training_class_label_mapping_path)
+    dump_gzip(values,class_labels,training_gzip_file_name)
+    print "Dumping done for training data"
+
+    print "Dumping for validation data"
+    values , class_labels = prepare_validation_data(testing_files_path,testing_class_label_mapping_path)
+    dump_validation_gzip(values, validation_gzip_file_name)
+    write_validation_labels(class_labels,validation_class_labels_file)
 
     # print "Creating Valid Data"
     # split_training_data(training_gzip_file_name)
